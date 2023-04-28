ad65b2d5ba1954446d18b1f6ff413b3f554cda02
Endris M <EndrisMd@gmail.com>
2023-04-28 20:57:16 +0300

    More involved type annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,15 +3,13 @@
    to the function uUsing  Duck typing - first element of a sequence
    Hint: look into TypeVar
 '''
-from typing import Any, Sequence, Union
-
+from typing import Mapping, Any, Union, TypeVar, Optional
 
 T = TypeVar('T')
-Res = Union[Any, T]
-Def = Union[T, None]
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Optional[T] = None) -> Union[Any, T]:
     '''Retrieves a value from a dict using a given key.
     '''
     if key in dct:
